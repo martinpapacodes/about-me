98c374ca9037352eb2a33278f7d23c2b2a711fe0
Martin Papa <martin.papa@outlook.com>
2019-11-07 17:31:22 -0800

    finished question 7

--- js/app.js
+++ js/app.js
@@ -54,6 +54,54 @@ var fifthQuestion = prompt('Do you think I like the Amazon?');
         // console.log('You are absolutely correct ' + name + '. I do not like the Amazon. Too many mosquitoes.');
         alert('You are absolutely correct ' + name + '. I do not like the Amazon. Too many mosquitoes.');
     }
+    
+    var counter = 0;
+    while ( counter !== 4) {
+    
+        var sixthQuestion = parseInt(prompt('Guess my favorite number! (Pick between 1 and 50)'));
+            
+        if (sixthQuestion > 24) {
+            alert('Chill ' + name + '. Pick another number. Too high.');
+        } else if (sixthQuestion < 24 ) {
+            alert('Guess another number. Too low.');
+        } else if (sixthQuestion === 24) {
+            alert ('You are a genius ' + name + '. 24 is my favorite number!');
+			break;
+        } 
+        counter++; 
+    }
+
+    if (counter === 4) {
+			alert('You have reached the maximum attempt. My favorite number is 24. ');
+    }    
+
+    var counter = 0;
+    while ( counter !== 6) {
+        
+       
+        var iceCreamFlavors = ['vanilla', 'chocolate', 'strawberry', 'coconut', 'hazelnut', 'horchata'];
+
+        var seventhQuestion = prompt('What is my favorite ice cream flavor?[Vanilla, Chocolate, Strawberry, Coconut, Hazelnut, Horchata]').toLowerCase();
+        
+        for(var i = 0; i <iceCreamFlavors.length; i++) {
+            if (seventhQuestion === 'vanilla') {
+                alert('You are a correct. My favorite flavor is plain ole vanilla.');
+                break; 
+            } else {
+                alert('Keep trying.......');
+                break;
+            }   
+        }  
+
+        if (seventhQuestion === 'vanilla') {
+            break;
+        } 
+        counter++; 
+    }
+    
+    if (counter === 6) {
+        alert('Maximum attempt reached. My favorite ice cream is Vanilla!');
+    }
 
 
-alert('Thank you for participating ' + name + '.');
+    alert('Thank you for participating ' + name + '.');
